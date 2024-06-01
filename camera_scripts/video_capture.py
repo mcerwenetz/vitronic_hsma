a58f9fa7719be2109a65eede6c0e630cae52e66c
marius cerwenetz <1824006@stud.hs-mannheim.de>
2024-06-01 11:29:24 +0200

    added video detection

--- camera_scripts/video_capture.py
+++ camera_scripts/video_capture.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
 import picamera2
+# import time
 
-MAX_ACTIVITY_RATION_THRESHOLD = 0.2
+MAX_ACTIVITY_RATION_THRESHOLD = 0.3
 LEARN_ITERATIONS=20
 
 def learn_mask(background_subtractor, camera):
@@ -17,15 +18,18 @@ def learn_mask(background_subtractor, camera):
     print("finished learning")
     return max_activity
 
-def check_activity(background_subtractor, camera, max_activity):
+def check_activity(background_subtractor, camera, max_activity, orb):
     while(True):
         im = camera.capture_array()
         fgmask = background_subtractor.apply(im)
         current_activity = np.count_nonzero(fgmask)
-        print(current_activity)
         if current_activity > max_activity*MAX_ACTIVITY_RATION_THRESHOLD:
             print("activity detected")
-
+            im = cv2.bitwise_and(im, im, mask=fgmask)
+            im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            kp, des = orb.detectAndCompute(im, None)
+            im = cv2.drawKeypoints(im, kp, None, color=(0,255,0), flags=None)
+            cv2.imwrite("detected_image.jpg",im)
 
 
 def main():
@@ -34,9 +38,11 @@ def main():
         main={"format": 'XRGB8888', "size": (3780, 2464)}))
     camera.set_controls({"ExposureTime": 1000})
     camera.start()
+    orb = cv2.ORB.create()
+
     background_subtractor = cv2.bgsegm.createBackgroundSubtractorCNT()
     max_activity = learn_mask(background_subtractor, camera)
-    check_activity(background_subtractor, camera, max_activity)
+    check_activity(background_subtractor, camera, max_activity, orb)
 
 
 if __name__ == '__main__':
