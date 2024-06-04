06943f73d2916655df314d780935415ba1b9a089
lasse.ellsaesser@web.de <lasse.ellsaesser@web.de>
2024-06-04 10:28:33 +0200

    0

--- camera_scripts/feature_gen.py
+++ camera_scripts/feature_gen.py
@@ -112,7 +112,7 @@ def main():
             if line == "interrupted light barrier":
                 print("[INFO] package detected")
                 pc +=1
-                os.mkdir(f"{pc}_data")
+                os.makedirs(f"{pc}_data",exist_ok=True)
                 img_list = []
                 ts1 = time()
                 for i in range(num_pics):
