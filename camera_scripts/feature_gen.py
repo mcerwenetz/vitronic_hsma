057dc207b934a14dfd953e2de9e0e1595b6d3e41
lasse.ellsaesser@web.de <lasse.ellsaesser@web.de>
2024-06-05 18:20:19 +0200

    0

--- camera_scripts/feature_gen.py
+++ camera_scripts/feature_gen.py
@@ -61,7 +61,6 @@ def orb_func(orb,im,fgmask):
     im = np.array(im)
     im = cv2.bitwise_and(im,im, mask=fgmask)
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    cv2.imshow(im)
     kp, des = orb.detectAndCompute(im, None)
     return des
 
