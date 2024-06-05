f9a21400942ac8600d7d1cf0f54efcc67494ad99
lasse.ellsaesser@web.de <lasse.ellsaesser@web.de>
2024-06-05 17:34:22 +0200

    0

--- camera_scripts/sql_funcs.py
+++ camera_scripts/sql_funcs.py
@@ -113,7 +113,7 @@ def addEntry(connection, cursor, gate, classification, features:np.ndarray, leng
             id = val[0]
             feature = np.frombuffer(val[1],dtype=np.int8)
             feature.reshape(500,32)
-
+            print(type(feature))
             matches = bf.knnMatch(features, feature, k=2) #features is the new classified image feature vector and feature is the feature vec of an old db entry
             dic[id] = 0
             for m , n in matches:
