24573a61038f80b57ad7f21f97ea44ea8c1006ea
Lasse Ellsässer <1921706@stud.hs-mannheim.de>
2024-06-07 16:50:32 +0200

    0

--- camera_scripts/getPhotos.py
+++ camera_scripts/getPhotos.py
@@ -1,6 +1,5 @@
 from picamera2 import Picamera2
-
-
+import subprocess
 def main():
 
     camera = Picamera2()
@@ -11,12 +10,15 @@ def main():
     i = 0
     while True:
 
+	
         inp = input("press enter to take a photo. press e to exit.")
         if inp == "e":
             break
         img = camera.capture_image()
         img.save(f"{i}.jpg")
-        i+=1
+        p = subprocess.Popen([f"scp {i}.jpg ela@hostname:/home/ela/Desktop/"])
+	sts = p.wait()
+        
 
 
 
